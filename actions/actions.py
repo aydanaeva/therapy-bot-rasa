4afb0e4502479faa4ae8b896503f0dc890582e66
aydan <aydanaeva@gmail.com>
2021-06-09 08:30:06 +0200

    added stories

--- actions/actions.py
+++ actions/actions.py
@@ -8,20 +8,32 @@
 # This is a simple example for a custom action which utters "Hello World!"
 
 # from typing import Any, Text, Dict, List
-#
+
 # from rasa_sdk import Action, Tracker
 # from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
+
+
+# class Actions(Action):
+
+#     def greet_by_name(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         dispatcher.utter_message(
+#                     template="utter_greet",
+#                     name="Sara"
+#                     )
+#         return []
+
+
 #     def name(self) -> Text:
 #         return "action_hello_world"
-#
+
 #     def run(self, dispatcher: CollectingDispatcher,
 #             tracker: Tracker,
 #             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
+
 #         dispatcher.utter_message(text="Hello World!")
-#
+
 #         return []
+
